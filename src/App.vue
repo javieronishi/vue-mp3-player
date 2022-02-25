629962a2b0700b3b7b4ecb1f8b461c2600f38235
Javier Onishi Sadud <javier-onishi@outlook.com>
2022-02-25 15:26:23 -0400

    para ti

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <a href="https://www.renacer.com.bo/obituariosDetalle.aspx?idObituario=53">Para ti!!</a>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  components: {},
+
+  data: () => ({
+    //
+  }),
+};
+</script>

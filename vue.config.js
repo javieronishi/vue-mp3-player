437d47736ba0048be249f86bda8e8b31dbe16a5e
Javier Onishi <javier-onishi@outlook.com>
2022-04-19 00:09:43 -0400

    agregando config para la pagina

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,5 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
   lintOnSave: false,
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-mp3-player/" : "/",
 });
